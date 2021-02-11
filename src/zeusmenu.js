29b36f41dfc6a404528f892a1b321a4c4162ca33
ishiLucas <lucasleandro78891@gmail.com>
2021-02-11 18:51:43 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,12 @@
+const texismenu = (prefix, pushname) => {
+    return `❍ *Comandos do Texis*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.texismenu = texismenu
